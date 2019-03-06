3d0ac5d36c4c170d16f91f1d400e28056c3acc7c
Chase Clark <cclark42@uic.edu>
2019-03-06 09:55:07 -0600

    Delete todo.R

--- R/todo.R
+++
@@ -1,6 +0,0 @@
-# Make this dynamic
-#
-# https://github.com/electron/electron-quick-start/blob/master/package.jsonhttps://github.com/electron/electron-quick-start/blob/master/package.json
-#
-# https://github.com/electron/electron-quick-start/blob/master/package-lock.json
-# #
