73c4cbec39bb2cc44837a0bbc33d75a5590ed2ce
Chase Clark <cclark42@uic.edu>
2019-03-28 16:39:28 -0500

    skip getting node test on mac linux

--- tests/testthat/test-get_nodejs.R
+++ tests/testthat/test-get_nodejs.R
@@ -17,6 +17,7 @@ if(dir.exists(temp)) {
 
 
 test_that("get_nodejs provides message", {
+  skip_on_os(c("mac","linux"))
   expect_message(get_nodejs(nodeUrl = "https://nodejs.org/dist",
                             installTo = temp,
                             force = FALSE,
@@ -24,15 +25,16 @@ test_that("get_nodejs provides message", {
 })
 
 
-a <- get_nodejs(nodeUrl = "https://nodejs.org/dist",
-           installTo = temp,
-           force = FALSE,
-           nodeVersion = "v10.15.1")
 
 
 
 test_that("get_nodejs provides paths for node and npm", {
+  skip_on_os(c("mac","linux"))
 
+  a <- get_nodejs(nodeUrl = "https://nodejs.org/dist",
+                  installTo = temp,
+                  force = FALSE,
+                  nodeVersion = "v10.15.1")
   expect_equal(length(a), 2L)
   expect_equal(names(a), c("nodePath", "npmPath"))
   expect_equal(class(unlist(a)), "character")
