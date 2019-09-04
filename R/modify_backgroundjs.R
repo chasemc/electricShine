919c01fac6f9c074d96bbce3d3a8d13b23670b25
chase <cclark42@uic.edu>
2019-09-03 21:30:11 -0500

    change names, fix formatting  for modify_background_js()

--- R/modify_backgroundjs.R
+++ R/modify_backgroundjs.R
@@ -2,7 +2,7 @@
 #' Modify background.js to include the call to the shiny app
 #'
 #' @param background_js_path path to the final background.js, not the one in inst/...
-#' @param package_name package name, will be used for namespacing- (e.g. 'dplyr' in 'dplyr::filter()')
+#' @param my_package_name package name, will be used for namespacing- (e.g. 'dplyr' in 'dplyr::filter()')
 #' @param function_name function that runs your shiny app - (e.g. 'filter' in 'dplyr::filter()')
 #' @param r_path path from "r_lang" folder to the R/Rscript executable
 #'
@@ -10,25 +10,27 @@
 #' @export
 #'
 modify_background_js <- function(background_js_path,
-                                 package_name,
+                                 my_package_name,
                                  function_name,
                                  r_path){
   if (!file.exists(background_js_path)) {
     stop("addFunctionToBackgroundJs() failed because background_js_path didn't point to an existing file.")
   }
   
-  b <- readLines(background_js_path)
+  background_js_contents <- readLines(background_js_path)
   
-  R_SHINY_FUNCTION <- paste0(package_name, 
+  R_SHINY_FUNCTION <- paste0(my_package_name, 
                              "::",
                              function_name)
   
+  background_js_contents <- sapply(background_js_contents,
+                                   function(x) {
+                                     glue::glue(x, 
+                                                .open = "<?<",
+                                                .close = ">?>")
+                                   })
   
-  b <- sapply(b, function(x) glue::glue(x, 
-                                        .open = "<?<",
-                                        .close = ">?>")
-  )
-  
-  writeLines(b, background_js_path)
+  writeLines(background_js_contents,
+             background_js_path)
   
 }
