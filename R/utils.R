67fd32e98f863fd7941b4ead4e037258b437369f
Chase Clark <cclark42@uic.edu>
2019-09-04 15:53:34 -0500

    add check for node and npm (system() call)

--- R/utils.R
+++ R/utils.R
@@ -11,17 +11,17 @@
 write_text <- function(text,
                        filename,
                        path){
-
+  
   path <- base::file.path(path,
                           filename)
   
   path <- normalizePath(path, 
                         winslash = "/", 
                         mustWork = FALSE)
-
+  
   base::writeLines(text,
                    path)
-
+  
   if (file.exists(path)) {
     base::message(glue::glue("Successfully created {path}"))
   } else {
@@ -39,7 +39,7 @@ write_text <- function(text,
 #' @export
 #'
 create_folder <- function(app_root_path){
-
+  
   name <- base::basename(app_root_path)
   
   if (file.exists(app_root_path)) {
@@ -50,4 +50,115 @@ create_folder <- function(app_root_path){
   
 }
 
+
+
+
+
+#' Check if Node works
+#'
+#' @param node_top_dir directory containing nodejs app/exe
+#' @param expected_version  expected version of node
+#'
+#' @return TRUE/FALSE whether node exists and is functional
+.check_node_works <- function(node_top_dir,
+                              expected_version) {
+  
+  if (!grepl("v", expected_version)) {
+    expected_version <- paste0("v",
+                               expected_version)
+  }
+  
+  os <- electricShine::get_os()
+  
+  command <- normalizePath(node_top_dir,
+                           "/")
+  
+  if (os == "win") {
+    
+    command <- file.path(command,
+                         "node.exe")
+    
+  } else {
+    
+    command <- file.path(command,
+                         "node")    
+  }
+  
+  
+  
+  node_exists <- file.exists(command)
+  
+  if (node_exists) {
+    # Check that the node version is the same as what we expect
+    command <- paste0(command, 
+                      " -v")
+    
+    result <- tryCatch(system(command,
+                              intern = T),
+                       error = function(e) "not_found", 
+                       warning = function(e) "not_found")
+    
+    if (expected_version != result) {
+      
+      stop(glue::glue("Node executable in {node_top_dir} was found to be version {result}, but {expected_version} was specified in electricShine."))
+      
+    }
+    node_exists <- TRUE 
+  }
+  
+  return(node_exists)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#' Check if npm works
+#'
+#' @param node_top_dir directory containing npmjs app/exe
+#' @param expected_version  expected version of npm
+#'
+#' @return TRUE/FALSE whether npm exists and is functional
+.check_npm_works <- function(node_top_dir) {
+  
+  os <- electricShine::get_os()
+  
+  command <- normalizePath(node_top_dir,
+                           "/", 
+                           mustWork = FALSE)
+  
+  
+  
+  command <- file.path(command,
+                       "npm")  
+  
+  npm_exists <- file.exists(command)
+  
+  if (npm_exists) {
+    # Check that the npm version is the same as what we expect
+    command <- paste0(command, 
+                      " -v")
+    
+    result <- tryCatch(system(command,
+                              intern = T),
+                       error = function(e) FALSE, 
+                       warning = function(e) FALSE)
+    
+    if(!isFALSE(result)) { 
+      
+      npm_exists <- TRUE 
+    }
+  }
+  
+  return(npm_exists)
+}
 
