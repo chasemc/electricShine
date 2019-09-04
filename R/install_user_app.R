92499caeb052b151593b47a87dc15487c97e763f
chase <cclark42@uic.edu>
2019-09-03 22:39:47 -0500

    simplify  install_user_app()

--- R/install_user_app.R
+++ R/install_user_app.R
@@ -1,8 +1,8 @@
 #' Install shiny app package and dependencies
 #'
 #' @param library_path path to the new Electron app's R's library folder
-#' @param site {remotes} package function, one of c("github", "gitlab", "bitbucket", "local")
-#' @param repo e.g. if site is github: "chasemc/demoApp" ; if site is local: "C:/Users/chase/demoApp"
+#' @param repo_location {remotes} package function, one of c("github", "gitlab", "bitbucket", "local")
+#' @param repo e.g. if repo_location is github: "chasemc/demoApp" ; if repo_location is local: "C:/Users/chase/demoApp"
 #' @param repos cran like repository package dependencies will be retrieved from  
 #' @param package_install_opts further arguments to remotes::install_github, install_gitlab, install_bitbucket, or install_local
 #'
@@ -14,29 +14,29 @@
 #' 
 #'\dontrun{
 #'
-#' install_user_app(site = "github", 
+#' install_user_app(repo_location = "github", 
 #'                  repo = "chasemc/demoApp@@d81fff0")
 #'                  
-#' install_user_app(site = "github", 
+#' install_user_app(repo_location = "github", 
 #'                  repo = "chasemc/demoApp@@d81fff0",
 #'                  auth_token = "my_secret_token")                  
 #'                  
-#' install_user_app(site = "bitbucket", 
+#' install_user_app(repo_location = "bitbucket", 
 #'                  repo = "chasemc/demoApp",
 #'                  auth_user = bitbucket_user(), 
 #'                  password = bitbucket_password())        
 #'                  
-#' install_user_app(site = "gitlab", 
+#' install_user_app(repo_location = "gitlab", 
 #'                  repo = "chasemc/demoApp",
 #'                  auth_token = "my_secret_token")   
 
-#' install_user_app(site = "local", 
+#' install_user_app(repo_location = "local", 
 #'                  repo = "C:/Users/chase/demoApp",
 #'                  build_vignettes = TRUE)                                      
 #' }
 #' 
 install_user_app <- function(library_path = NULL,
-                             site = "github",
+                             repo_location = "github",
                              repo = "chasemc/IDBacApp",
                              repos = cran_like_url,
                              package_install_opts = NULL){
@@ -44,20 +44,20 @@ install_user_app <- function(library_path = NULL,
   accepted_sites <- c("github", "gitlab", "bitbucket", "local")
   
   
-  if(is.null(library_path)) {
+  if (is.null(library_path)) {
     stop("install_user_app() requires library_path to be set.")
   }
   
-  if(file.exists(library_path)) {
+  if (!dir.exists(library_path)) {
     stop("install_user_app() library_path wasn't found.")
   }
   
-  if (length(site) != 1L) {
-    stop(glue::glue("install_user_app(site) must be character vector of length 1"))
+  if (length(repo_location) != 1L) {
+    stop(glue::glue("install_user_app(repo_location) must be character vector of length 1"))
   }
   
-  if (!site %in% accepted_sites) {
-    stop(glue::glue("install_user_app(site) must be one of: {accepted_sites}"))
+  if (!repo_location %in% accepted_sites) {
+    stop(glue::glue("install_user_app(repo_location) must be one of: {accepted_sites}"))
   }
   
   if (!nchar(repo) > 0) {
@@ -71,39 +71,16 @@ install_user_app <- function(library_path = NULL,
     }
   }
   
+  remotes_code <- as.character(glue::glue("install_{repo_location}"))
   
-  if (length(package_install_opts) > 0) {
-    
-    provided_args_names <- names(package_install_opts)
-    
-    fun_args <- names(formals(eval(parse(text = glue::glue("remotes::install_{site}")))))
-    
-    package_install_opts <- package_install_opts[provided_args_names %in% fun_args]
-    
-    valid_args <- paste0(names(package_install_opts), 
-                         sep = " = ")
-    
-    valid_args <- paste0(valid_args, 
-                         package_install_opts,
-                         collapse = ", \n ")
-    
-    remotes_code <- glue::glue("remotes::install_{site}('{repo}', {valid_args})")
-    
-    
-    
-    withr::with_libpaths(library_path,
-                         eval(parse(text = remotes_code))
-    )
-    
-  } else {
-    
-    remotes_code <- glue::glue("remotes::install_{site}('{repo}')")
-    
-    withr::with_libpaths(library_path,
-    eval(parse(text = remotes_code))
-    )
-  }
+  remotes_code <- getFromNamespace(remotes_code, ns = "remotes")
   
+  repo <- as.list(repo)
   
+  passthr <- c(repo, repos = repos ,package_install_opts)
+  
+  withr::with_libpaths(library_path,
+                       do.call(remotes_code, passthr)
+  )
   
 }
