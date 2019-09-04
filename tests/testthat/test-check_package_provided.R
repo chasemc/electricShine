16f83381ce3f57250e070e2eb7978b0ce72734d6
Chase Clark <cclark42@uic.edu>
2019-09-04 15:52:58 -0500

    update tests for .check_package_provided()

--- tests/testthat/test-check_package_provided.R
+++ tests/testthat/test-check_package_provided.R
@@ -15,14 +15,11 @@ test_that(".check_package_provided works", {
                                        local_package_path = NULL),
                "Must provide both 'git_host' and 'git_repo', or just 'local_package_path'.",
                fixed = TRUE)
-  
-  expect_error(.check_package_provided(github_repo = "NULL",
-                                       local_package = "NULL"),
-               "Values provided for both 'github_repo' and 'local_package'; electricShine requires that only one of these is not NULL.",
-               fixed = TRUE)
+  expect_error(
+    .check_package_provided(git_host = "any_string",
+                            git_repo = "any_string",
+                            local_package_path = "any_string"),
+    "Values provided for both 'git_repo' and 'local_package_path'; electricShine requires that only one of these is not NULL.",
+    fixed = TRUE)
 })
 
-
-.check_package_provided(git_host = "github",
-                        git_repo = "chasemc",
-                        local_package_path = NUll)
