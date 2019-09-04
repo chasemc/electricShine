18d0cee15c876d576f5bc5c07a2e8c24cd670151
chase <cclark42@uic.edu>
2019-09-03 21:24:18 -0500

    add fail checks so can fail metafunction early

--- R/fail_fast.R
+++ R/fail_fast.R
@@ -1,27 +1,54 @@
-#' Check that repo is set
+
+
+#' Check that a repo for packages/R was set
 #'
-#'    Doesn't check whether it works, only that it is not-null
+#' @param cran_like_url cran_like_url
+#' @param mran_date mran_date
 #'
-#' @param arguments arguments to check 
-#'
-#' @return stops if neither cran-like url or mran date is set
-.check_repo_set <- function(arguments){
+.check_repo_set <- function(cran_like_url = NULL, 
+                            mran_date = NULL) {
   
-  cran_like_url <- arguments$cran_like_url
-  mran_date <- arguments$mran_date
-  
-  # Either 'cran_like_url' or 'mran_date' must be set
+  # Either 'cran_like_url' or 'local_package_path_path' must be set
   if (is.null(c(cran_like_url, mran_date))) {
-    base::stop("'cran_like_url' or 'mran_date' must be set. 'mran_date' is suggested and should be a date in the format 'yyyy-mm-dd' ") 
+    base::stop("electricShine requires you to specify either a 'cran_like_url' or 'mran_date' argument specifying
+         the shiny app/package to be turned into an Electron app") 
   }
   
   # Ensure that 'cran_like_url' and 'mran_date' aren't set at the same time.
   if (!is.null(cran_like_url) && !is.null(mran_date)) {
-    stop("Values provided for both 'cran_like_url' and 'mran_date'.") 
+    stop("Values provided for both 'cran_like_url' and 'mran_date'; electricShine requires that only one of these is not NULL.") 
   }
   
 }
 
+
+
+
+
+#' Title
+#'
+#' @param cran_like_url 
+#'
+.ping_url <- function(cran_like_url){
+  
+  if(capabilities("libcurl")) {
+    
+    a <- base::curlGetHeaders(cran_like_url)
+    
+    if (attributes(a)$status != 200L) {
+      message(a)
+      stop("provided cran_like_url was unable to resolve host ")
+      
+    }
+    message("----------")
+    message(paste0("Connection to: ", cran_like_url, "\n"))
+    message(a[1:10])
+    message("----------")
+  } else {
+    warning("libcurl unavailable to check url before downloading R and R packages.")
+  }
+  
+}
 
 
 #' Check if compatible architecture
@@ -30,22 +57,20 @@
 #'
 #' @return Stops if unsupported architecture
 .check_arch <- function(arch = base::version$arch[[1]]){
- 
-   if (arch != "x86_64") {
+  
+  if (arch != "x86_64") {
     base::stop("Unfortunately 32 bit operating system builds are unsupported, if you would like to contribute to support this, that would be cool")
-   }
+  }
   
 }
 
 
 #' Check whether build path exists
 #'
-#' @param arguments arguments to check 
+#' @param build_path build_path
 #'
 #' @return stops 
-.check_build_path_exists <- function(arguments){
-  
-  build_path <- arguments$build_path
+.check_build_path_exists <- function(build_path){
   
   if (is.null(build_path)) {
     base::stop("'build_path' not provided")
@@ -54,7 +79,7 @@
   if (!is.character(build_path)) {
     base::stop("'build_path' should be character type.")
   }
- 
+  
   if (!dir.exists(build_path)) {
     base::stop("'build_path' provided, but path wasn't found.")
   }
@@ -62,29 +87,37 @@
 
 
 
-
-
 #' Check package paths
 #'
 #'    Doesn't check whether it works, only for conflicts in arguments
 #'
-#' @param arguments arguments to check 
+#' @param git_host git_host 
+#' @param git_repo git_repo 
+#' @param local_package_path local_package_path 
 #'
 #' @return stops 
-.check_package_provided <- function(arguments){
+.check_package_provided <- function(git_host,
+                                    git_repo,
+                                    local_package_path_path){
   
-  github_repo <- arguments$github_repo
-  local_package <- arguments$local_package
-  
-  # Either 'github_repo' or 'local_package' must be set
-  if (is.null(c(github_repo, local_package))) {
-    base::stop("electricShine requires you to specify either a 'github_repo' or 'local_package' argument specifying
+  # Either 'git_repo' or 'local_package_path_path' must be set
+  if (is.null(c(git_repo, local_package_path))) {
+    base::stop("electricShine requires you to specify either a 'git_repo' or 'local_package_path' argument specifying
          the shiny app/package to be turned into an Electron app") 
   }
   
-  # Ensure that 'github_repo' and 'local_package' aren't set at the same time.
-  if (!is.null(github_repo) && !is.null(local_package)) {
-    stop("Values provided for both 'github_repo' and 'local_package'; electricShine requires that only one of these is not NULL.") 
+  # Ensure that 'git_repo' and 'local_package_path' aren't set at the same time.
+  if (!is.null(git_repo) && !is.null(local_package_path)) {
+    stop("Values provided for both 'git_repo' and 'local_package_path'; electricShine requires that only one of these is not NULL.") 
+  }
+  
+  # Ensure that 'git_host' and 'local_package_path' aren't set at the same time.
+  if (!is.null(git_host) && !is.null(local_package_path)) {
+    stop("Values provided for both 'git_host' and 'local_package_path'; electricShine requires that only one of these is not NULL.") 
+  }
+  
+  if (sum(is.null(git_host), is.null(git_repo)) == 1) {
+    stop("Must provide both 'git_host' and 'git_repo', or just 'local_package_path'.") 
   }
   
 }
