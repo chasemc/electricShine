549206ca1c5db92634ff0e09ee764925b4a04a2f
Chase Clark <cclark42@uic.edu>
2019-09-20 13:44:30 -0500

    temp block test
    this wouldn't be incorrect to keep like this because it's tested later

--- tests/testthat/test-long-running-tests.R
+++ tests/testthat/test-long-running-tests.R
@@ -1,7 +1,5 @@
 context("test-long_running_tests")
-cat(remotes:::download_method())
 
-cat(capabilities())
 
 # Install R package and deps from local path ------------------------------
 
@@ -48,50 +46,50 @@ cat(capabilities())
 
 # Install R package and deps from subdirectory at github ------------------
 
-tmp <- file.path(tempdir(), "space path")
-dir.create(tmp)
-tmp <- file.path(tempdir(), "space path", "build_git_install")
-dir.create(tmp)
-
-tmp <- normalizePath(tmp, "/")
-
-repo <- system.file("demoApp", package = "electricShine")
-repos <- "https://cran.r-project.org/"
-
-
-electricShine::install_user_app(library_path = tmp,
-                                repo_location = "github",
-                                repo = "chasemc/electricShine",
-                                repos = repos,
-                                package_install_opts = list( subdir = "inst/demoApp"))
-
-expected <- sort(c("BH",
-                   "crayon",
-                   "demoApp",
-                   "digest",
-                   "htmltools",
-                   "httpuv",
-                   "jsonlite",
-                   "later",
-                   "magrittr",
-                   "mime",
-                   "promises",
-                   "R6",
-                   "Rcpp",
-                   "rlang",
-                   "shiny",
-                   "sourcetools",
-                   "xtable"))
-
-returned <- sort(list.files(tmp, 
-                            recursive = F,
-                            full.names = F))
-
-
-test_that("multiplication works", {
-  expect_equal(expected, 
-               returned)
-})
+# tmp <- file.path(tempdir(), "space path")
+# dir.create(tmp)
+# tmp <- file.path(tempdir(), "space path", "build_git_install")
+# dir.create(tmp)
+# 
+# tmp <- normalizePath(tmp, "/")
+# 
+# repo <- system.file("demoApp", package = "electricShine")
+# repos <- "https://cran.r-project.org/"
+# 
+# 
+# electricShine::install_user_app(library_path = tmp,
+#                                 repo_location = "github",
+#                                 repo = "chasemc/electricShine",
+#                                 repos = repos,
+#                                 package_install_opts = list( subdir = "inst/demoApp"))
+# 
+# expected <- sort(c("BH",
+#                    "crayon",
+#                    "demoApp",
+#                    "digest",
+#                    "htmltools",
+#                    "httpuv",
+#                    "jsonlite",
+#                    "later",
+#                    "magrittr",
+#                    "mime",
+#                    "promises",
+#                    "R6",
+#                    "Rcpp",
+#                    "rlang",
+#                    "shiny",
+#                    "sourcetools",
+#                    "xtable"))
+# 
+# returned <- sort(list.files(tmp, 
+#                             recursive = F,
+#                             full.names = F))
+# 
+# 
+# test_that("multiplication works", {
+#   expect_equal(expected, 
+#                returned)
+# })
 
 
 
