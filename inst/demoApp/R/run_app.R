ad6bacc10014ac0fe751c72e11fd16a1c81cde74
Chase Clark <cclark42@uic.edu>
2019-10-27 18:17:19 -0500

    update demoApp

--- inst/demoApp/R/run_app.R
+++ inst/demoApp/R/run_app.R
@@ -1,11 +1,10 @@
 #' Run the Shiny Application
 #'
 #' @param options optional, described in ?shiny::shinyApp
-#' @param ... arguments to pass to golem_opts
 #'
 #' @export
 run_app <- function(options = list()) {
-  shinyApp(ui = app_ui,
+  shiny::shinyApp(ui = app_ui,
            server = app_server,
            options = options) 
   
